4136ff8f0dac1dda1ed96760a63c857eeb0d38a9
MartianMooCow <NathenL2832@gmail.com>
2023-05-16 23:31:53 -0400

    Added footer component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 import Header from './components/Header'
 import Home from './components/Home'
+import Footer from './components/Footer'
 import "./styles/index.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
@@ -10,5 +11,6 @@ root.render(
     <React.StrictMode>
         <Header/>
         <Home/>
+        <Footer/>
     </React.StrictMode>
 )
