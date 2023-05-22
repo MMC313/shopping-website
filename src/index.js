3ab69e8829fa275475f6f7620ce27a77a2393c1d
MartianMooCow <NathenL2832@gmail.com>
2023-05-22 16:41:30 -0400

    added FootNav section

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 import Header from './components/Header'
-import Home from './components/Home'
+import FooterNav from './components/FooterNav'
 import Footer from './components/Footer'
+import RouteSwitch from './RouteSwitch'
 import "./styles/index.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
@@ -10,7 +11,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
     <React.StrictMode>
         <Header/>
-        <Home/>
+        <RouteSwitch/>
+        <FooterNav/>
         <Footer/>
     </React.StrictMode>
 )
