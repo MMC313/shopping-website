1f432b2ec581f8d4ce4760bf311d0798b63211a0
MartianMooCow <NathenL2832@gmail.com>
2023-06-05 11:00:32 -0400

    initial commit

---
+++ src/components/Cart.js
@@ -0,0 +1,44 @@
+import '../styles/Cart.css'
+import close from '../images/x.svg' 
+
+
+function Cart(props){
+    let toggleCart = props.toggleCart
+
+    function closeCart(){
+        let cartContainer = document.getElementById("cart-component")
+        cartContainer.classList.remove("cart-shown")
+        cartContainer.classList.add("cart-hidden-animation")
+        setTimeout(()=>{
+            cartContainer.classList.remove("cart-hidden-animation")
+            }
+            ,500
+        )
+    }
+
+    return(
+        <div id='cart-component' className='cart-hidden'>
+            <div className='cart-btn-container'><button className='cartCloseBtn' onClick={closeCart}><img className='cartCloseIcon' src={close} alt="Close" /></button></div>
+            <div className='cart-shopping-container'>
+                <div className='cart-shopping-title'>Shopping Bag</div>
+                <div className='cart-list'>
+                    <div>Stuff I want to buy</div>
+                    <div>Stuff I want to buy</div>
+                    <div>Stuff I want to buy</div>
+                    <div>Stuff I want to buy</div>
+                    <div>Stuff I want to buy</div>
+                    <div>Stuff I want to buy</div>
+                    <div>Stuff I want to buy</div>
+                    <div>Stuff I want to buy</div>
+                    <div>Stuff I want to buy</div>
+                    <div>Stuff I want to buy</div>
+                  
+
+                </div>
+                <button className='cart-checkout-btn'>CHECKOUT</button>
+            </div>
+        </div>
+    )
+}
+
+export default Cart;
