650e113a0e8232435dfa73f81412b41d18792a2a
MartianMooCow <NathenL2832@gmail.com>
2023-06-27 16:08:44 -0400

    updated filter buttons to return db data

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,16 +1,22 @@
 import '../styles/Shop.css'
 
 
-function changeTitle(event){
-    let shopTitle = document.querySelector(".shop-nav-title-current")
 
-    shopTitle.textContent = event.target.innerHTML
 
-    console.log(event.target.innerHTML)
-   
-}
 
-function Shop(){
+function Shop(props){
+
+    async function update(event){
+        let testData = await props.props
+        let shopTitle = document.querySelector(".shop-nav-title-current")
+    
+
+        console.log(event.target.id)
+        shopTitle.textContent = event.target.innerHTML
+        console.log(testData)
+        return testData
+       
+    }
 
 
     return(
@@ -21,20 +27,15 @@ function Shop(){
                     <div className='shop-nav-title-current'>All Graphics Cards</div>
                 </div>
                 <ul className='shop-nav-filter'>
-                    <li><button onClick={changeTitle} className='shop-nav-filter-btn'>All Graphics Cards</button></li>
-                    <li><button onClick={changeTitle} className='shop-nav-filter-btn'>NVIDIA RTX 3000 Series</button></li>
-                    <li><button onClick={changeTitle} className='shop-nav-filter-btn'>NVIDIA RTX 2000 Series</button></li>
-                    <li><button onClick={changeTitle} className='shop-nav-filter-btn'>AMD RX 7000 Series</button></li>
-                    <li><button onClick={changeTitle} className='shop-nav-filter-btn'>AMD RX 6000 series</button></li>
+                    <li><button onClick={update} className='shop-nav-filter-btn'>All Graphics Cards</button></li>
+                    <li><button onClick={update} id='GeForce 30' className='shop-nav-filter-btn'>NVIDIA RTX 3000 Series</button></li>
+                    <li><button onClick={update} id='GeForce 20' className='shop-nav-filter-btn'>NVIDIA RTX 2000 Series</button></li>
+                    <li><button onClick={update} className='shop-nav-filter-btn'>AMD RX 7000 Series</button></li>
+                    <li><button onClick={update} className='shop-nav-filter-btn'>AMD RX 6000 series</button></li>
                 </ul>
             </div>
-            <div className='shop-items'>
-                <div className='shop-item'>
-                    <img className='shop-item-image' src="lol" alt="" />
-                    <div className='shop-item-desc'></div>
-                    <div className='shop-item-price'></div>
-                    <button className='shop-item-buy-btn'></button>
-                </div>
+            <div className='items'>
+                
             </div>
         </div>
     )
