3d437f295a893332bdcaf483a94a64af6001b53d
MartianMooCow <NathenL2832@gmail.com>
2023-06-28 16:00:49 -0400

    changed port to support local enviroment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const port = 3000
+const PORT = process.env.PORT || 3000;
 const express = require('express')
 const cors = require('cors')
 const axios = require('axios')
@@ -51,4 +51,4 @@ app.get('/results', (req,res)=>{
   
 })
 
-app.listen(port, ()=> console.log(`Server running on port:${port}`)) 
+app.listen(PORT, ()=> console.log(`Server running on port:${PORT}`)) 
